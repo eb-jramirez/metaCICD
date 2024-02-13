d0cc122ca9d19bc467d4d6b080e7608efe432bd7
eb-jramirez <106706282+eb-jramirez@users.noreply.github.com>
2024-02-13 09:18:16 -0600

    env matrix  sequence for ihub deploy

--- metaCICD/Program.cs
+++ metaCICD/Program.cs
@@ -1,24 +1,4 @@
-﻿// using System;
-//
-// namespace CiCdDemo
-// {
-//     class Program
-//     {
-//         static void Main(string[] args)
-//         {
-//             string environment = args[0];
-//             Console.WriteLine($"Deploying {environment} environment...");
-//
-//             // Simulate deployment process
-//             System.Threading.Thread.Sleep(2000);
-//
-//             // Print timestamp
-//             Console.WriteLine($"Environment {environment} deployed successfully at {DateTime.Now}");
-//         }
-//     }
-// }
-
-using System;
+﻿using System;
 
 namespace CiCdDemo
 {
